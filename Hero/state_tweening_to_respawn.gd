d71f9312662e1e7582e65458a84f579e02bb2775
caiolopezcomz <caiolopez@gmail.com>
2024-05-25 02:55:42 -0300

    improved tweening to respawn

--- Hero/state_tweening_to_respawn.gd
+++ Hero/state_tweening_to_respawn.gd
@@ -4,12 +4,13 @@ var water_prone = false
 var death_prone = false
 var tween: Tween
 
+
 func on_enter():
 	var destination = hero.original_position
 	if hero.current_checkpoint != null:
 		destination = hero.current_checkpoint.global_position
 	tween = get_tree().create_tween()
-	tween.tween_property(hero, "global_position", destination, 1)
+	tween.tween_property(hero, "global_position", destination, 0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 
 
 func on_process(_delta: float):
