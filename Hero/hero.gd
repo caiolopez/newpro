b954fd7487f63ea018c3a095ec77d828af0e0ecf
caiolopezcomz <caiolopez@gmail.com>
2024-05-28 19:17:42 -0300

    added quick fall accel. Adding type

--- Hero/hero.gd
+++ Hero/hero.gd
@@ -1,46 +1,46 @@
 extends CharacterBody2D
 
-@export var can_dive = false ## Whether the hero has the ability to dive into water instead of floating.
-@export var shoots_fire = false ## Whether the hero has the ability to shoot incendiary bullets, required to damage enemies that are immune to regular bullets.
-@export var SPEED = 600.0 ## The moving speed of the hero.
-@export var JUMP_VELOCITY = -1000.0 ## The speed the hero jumps when grounded.
-@export var VAULT_VELOCITY = -500.0 ## The speed the hero jumps when performing the vault. Vaulting is an assist that aims at standardize velocity.y when the hero climbs a wall all the way up.
-@export var HEADBUTT_THRESHOLD = -300
+@export var can_dive: bool = false ## Whether the hero has the ability to dive into water instead of floating.
+@export var shoots_fire: bool = false ## Whether the hero has the ability to shoot incendiary bullets, required to damage enemies that are immune to regular bullets.
+@export var SPEED: float = 600.0 ## The moving speed of the hero.
+@export var JUMP_VELOCITY: float  = -1000.0 ## The speed the hero jumps when grounded.
+@export var VAULT_VELOCITY: float = -500.0 ## The speed the hero jumps when performing the vault. Vaulting is an assist that aims at standardize velocity.y when the hero climbs a wall all the way up.
+@export var HEADBUTT_THRESHOLD_VEL: float = -300.0 ## The minimum upward velocity required for the headbutt assist to take place and snap the Hero.
 @export var WALLJUMP_VELOCITY = Vector2(1600, -600) ## The speed the hero walljumps away from a wall.
 @export var CLIMB_VELOCITY: float = -1000 ## The speed the hero jumps upward when jumping to the same side of the wall (Megaman-style walljump).
 @export var GLIDE_VELOCITY: float = 100 ## The speed at which the hero slowly descends when airborne and holding Jump.
 @export var GLIDE_X_DRAG: float = -3000 ## The rate at which it decelerates to normal SPEED after walljump.
-@export var BLUNDER_AIRBORNE_VELOCITY = Vector2(-2000, 0) ## The strenth of the recoil when blunderjumping on air.
-@export var BLUNDER_GROUNDED_VELOCITY = Vector2(-800, 0) ## The strenth of the recoil when blunderjumping on ground.
+@export var BLUNDER_AIRBORNE_VELOCITY = Vector2(-2000, 0) ## The strength of the recoil when blunderjumping on air.
+@export var BLUNDER_GROUNDED_VELOCITY = Vector2(-800, 0) ## The strength of the recoil when blunderjumping on ground.
 @export var BLUNDER_AIRBORNE_DURATION: float = 0.1 ## The duration of the recoil when blunderjumping on air.
 @export var BLUNDER_GROUNDED_DURATION: float = 0.05 ## The duration of the recoil when blunderjumping on ground.
 @export var BLUNDER_JUMP_VELOCITY: float = -1200 ## The speed the hero jumps after blundershooting airborne.
 @export var BLUNDER_JUMP_WATER_BOUNCE_VELOCITY: float = -800 ## The speed the hero jumps after falling on water while holding jump from a blunder jump. Like Kiddy Kong.
-@export var gravity: float = 2000
-@export var MAX_FALL_VEL_Y = 2000.0 ## The maximum downward speed when falling.
-@export var BUOYANCY = 100 ## The upward acceleration when underwater. Only affects state Floating.
-@export var ASCENDING_VELOCITY = -300.0 ## The Y speed at which the hero swims upwards when holding jump while underwater. CAN_DIVE must be set to true.
-@export var MAX_DESCENT_VEL_Y = 300 ## The maximum downward speed when diving (CAN_DIVE must be set to true).
+@export var GRAVITY: float = 2000 ## The standard downward force, in absolute values.
+@export var FAST_FALL_GRAVITY: float = 3000 ## The downward force, in absolute values, used in falling. Should be higher than GRAVITY, in order to make the Hero fall faster.
+@export var MAX_FALL_VEL_Y: float = 2000.0 ## The maximum downward speed when falling.
+@export var BUOYANCY: float = 100 ## The upward acceleration when underwater. Only affects state Floating.
+@export var ASCENDING_VELOCITY: float = -300.0 ## The Y speed at which the hero swims upwards when holding jump while underwater. CAN_DIVE must be set to true.
+@export var MAX_DESCENT_VEL_Y: float = 300 ## The maximum downward speed when diving (CAN_DIVE must be set to true).
 @export var state_machine: StateMachine ## The state machine that governs this player controller. Drag-and-drop the state-machine object to this field.
-@export var bullet_manager: Node
-@onready var original_position:= global_position
-@onready var shoulder_rc:= get_node("ShoulderRC")
-@onready var pelvis_rc:= get_node("PelvisRC")
-@onready var next_grd_height:= get_node("NextGrdHeight")
-@onready var headbutt_assist:= get_node("HeadbuttAssist")
+@onready var original_position: Vector2 = global_position
+@onready var shoulder_rc: RayCast2D = get_node("ShoulderRC")
+@onready var pelvis_rc: RayCast2D = get_node("PelvisRC")
+@onready var next_grd_height: RayCast2D = get_node("NextGrdHeight")
+@onready var headbutt_assist: RayCast2D = get_node("HeadbuttAssist")
 @onready var dmg_taker:= Utils.find_dmg_taker(self)
 const is_foe: bool = false ## Flag necessary for components that are shared between Hero and enemies.
 var current_checkpoint: Area2D
 var was_on_wall: bool ## For variable change caculation
 var was_on_floor: bool ## For variable change caculation
 var was_pushing_wall: bool  ## For variable change caculation
+var was_on_water: bool ## For variable change caculation
 var on_wall_value_just_changed: bool
 var is_just_on_floor: bool
 var is_just_pushing_wall: bool
 var just_stopped_pushing_wall: bool
-var was_on_water: bool ## For variable change caculation
 var facing_direction = 1
-var is_on_water = false
+var is_on_water: bool = false
 var is_just_on_water: bool
 var current_water: Water
 var last_water_surface: float
@@ -73,9 +73,9 @@ func _process(_delta):
 func _physics_process(_delta):
 	pass
 
-func step_grav(delta):
+func step_grav(delta, downward_accel := GRAVITY):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += downward_accel * delta
 		velocity.y = minf(velocity.y, MAX_FALL_VEL_Y)
 
 
@@ -129,13 +129,13 @@ func is_input_blunder_shoot() -> bool:
 
 
 func shoot_regular() -> Area2D:
-	return bullet_manager.create_bullet(facing_direction, position, Vector2(800, 0), false, shoots_fire)
+	return BulletManager.create_bullet(facing_direction, position, Vector2(800, 0), false, shoots_fire)
 
 
 func shoot_blunder(amount: int, interval_angle: float):
 	var top_angle =  (amount-1) * interval_angle / 2
 	for i in range(amount):
-		bullet_manager.create_bullet(facing_direction, position, Vector2(800, 0), false, shoots_fire, top_angle - interval_angle * i)
+		BulletManager.create_bullet(facing_direction, position, Vector2(800, 0), false, shoots_fire, top_angle - interval_angle * i)
 
 
 func check_value_change():
