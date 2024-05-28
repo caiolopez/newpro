57f8fe52731162517a982a414ae2d950b75e3e10
caiolopezcomz <caiolopez@gmail.com>
2024-05-27 23:21:56 -0300

    added debug info to all state timers

--- timer_debug.gd
+++ timer_debug.gd
@@ -1,8 +1,17 @@
 extends Timer
 
+var was_stopped:= true
+var just_started:= false
 
 func _ready():
 	connect("timeout", on_timeout)
+
+
+func _process(delta):
+	just_started = was_stopped and not is_stopped()
+	was_stopped = is_stopped()
+	if just_started:
+		print("TIMER ", name, " STARTED")
 
 
 func on_timeout():
