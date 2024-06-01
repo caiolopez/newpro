0a0ee6b9f0483f56c528b57c241a17e091651713
caiolopezcomz <caiolopez@gmail.com>
2024-06-01 19:20:38 -0300

    switches and doors fully working.

--- SwitchesAndDoors/switch.gd
+++ SwitchesAndDoors/switch.gd
@@ -6,29 +6,18 @@ enum SwitchState {
 	TEMP_ON
 }
 
-@export var req_simultaneous: bool = false ## Set this to true if all switches must be activated roughly at the same time.
-@export var simult_window_duration: float = 0.1 ## The amount of time before a temporarily on switch gives up and shuts back off.
 @export var toggle: bool = false ## Default switches (toggle == false) can only be switched on. Set this to true if this switch can be set to off as well.
+var controller: Node2D
 var current_state: SwitchState
-var related_switches: Array[Switch] = []
-var related_doors: Array[Door] = []
-var group_is_req: bool = false
-var group_is_simult: bool = false
-var group_is_on: bool = false
-var group_is_saved: bool = false
 
 
 func _ready():
-	check_group()
-	Events.reached_checkpoint.connect(commit_status)
-	Events.respawned_at_checkpoint.connect(reset_status)
+	controller = get_parent()
+	$TimerSimultWindow.set_wait_time(controller.simult_window_duration)
 	$TimerSimultWindow.timeout.connect(on_simult_window_timeout)
-	$TimerSimultWindow.set_wait_time(simult_window_duration)
 	area_entered.connect(on_bullet_entered)
 	switch_off()
 
-func _process(_delta):
-	print("group is on: ", group_is_on, " | group in saved: ", group_is_saved)
 
 func switch_on():
 	current_state = SwitchState.ON
@@ -51,22 +40,6 @@ func temporarily_switch_on():
 	modulate = Color(1, 1, 0)
 
 
-func commit_status():
-	if group_is_on:
-		group_is_saved = true
-
-
-func reset_status():
-	if not group_is_saved:
-		switch_off()
-		for door in related_doors:
-			door.insta_close()
-	else:
-		switch_off()
-		for door in related_doors:
-			door.insta_open()
-
-
 func on_simult_window_timeout():
 	switch_off()
 
@@ -77,55 +50,17 @@ func on_bullet_entered(area):
 		
 	match current_state:
 		SwitchState.OFF:
-			if group_is_simult:
+			if controller.req_simultaneous:
 				temporarily_switch_on()
-				var all_are_temp_on: bool = true
-				for sw in related_switches:
-					if sw.current_state != SwitchState.TEMP_ON:
-						all_are_temp_on = false
-				if all_are_temp_on:
-					turn_on_rel_switches()
-					open_rel_doors()
-			else:
-				turn_on_rel_switches()
-				open_rel_doors()
+			controller.on_switch_turned_on()
 		SwitchState.ON:
 			if toggle:
-				close_rel_doors()
-				turn_off_rel_switches()
+				controller.on_switch_turned_off()
 		SwitchState.TEMP_ON:
 			$TimerSimultWindow.start()
 		
 	area.kill_bullet()
 
 
-func check_group():
-	for sib in get_parent().get_children():
-		if sib is Switch:
-			related_switches.append(sib)
-			if sib.req_simultaneous:
-				group_is_simult = true
-		if sib is Door:
-			related_doors.append(sib)
 
 
-func open_rel_doors():
-	for door in related_doors:
-		door.open_d.emit()
-
-
-func close_rel_doors():
-	for door in related_doors:
-		door.close_d.emit()
-
-
-func turn_on_rel_switches():
-	for switch in related_switches:
-		switch.switch_on()
-		group_is_on = true
-
-
-func turn_off_rel_switches():
-	for switch in related_switches:
-		switch.switch_off()
-		group_is_on = false
--- SwitchesAndDoors/switch_group_controller.gd
+++ SwitchesAndDoors/switch_group_controller.gd
@@ -1,11 +1,90 @@
 extends Node2D
 
+enum SwitchState {
+	ON,
+	OFF,
+	TEMP_ON
+}
 
-# Called when the node enters the scene tree for the first time.
+@export var req_simultaneous: bool = false ## Set this to true if all switches must be activated at the same time.
+@export_range(0.01, 60) var simult_window_duration: float = 0.1 ## The amount of time before a temporarily on switch gives up and shuts back off.
+var related_switches: Array[Switch] = []
+var related_doors: Array[Door] = []
+var group_is_on: bool = false
+var group_is_saved: bool = false
+
+
 func _ready():
-	pass # Replace with function body.
+	check_group()
+	Events.reached_checkpoint.connect(commit_status)
+	Events.respawned_at_checkpoint.connect(reset_status)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func check_group():
+	for sib in get_children():
+		if sib is Switch:
+			related_switches.append(sib)
+		if sib is Door:
+			related_doors.append(sib)
+
+
+func commit_status():
+	if group_is_on:
+		group_is_saved = true
+
+
+func reset_status():
+	if not group_is_saved:
+		for sw in related_switches:
+			sw.switch_off()
+		for door in related_doors:
+			door.insta_close()
+	else:
+		for sw in related_switches:
+			sw.switch_on()
+		for door in related_doors:
+			door.insta_open()
+
+
+func open_rel_doors():
+	for door in related_doors:
+		door.open_d.emit()
+
+
+func close_rel_doors():
+	for door in related_doors:
+		door.close_d.emit()
+
+
+func turn_on_rel_switches():
+	for switch in related_switches:
+		switch.switch_on()
+		group_is_on = true
+
+
+func turn_off_rel_switches():
+	for switch in related_switches:
+		switch.switch_off()
+		group_is_on = false
+
+
+func on_switch_turned_on():
+	if req_simultaneous:
+		var all_temp: bool = true
+		for sw in related_switches:
+			if sw.current_state != SwitchState.TEMP_ON:
+				all_temp = false
+		if all_temp:
+			turn_on_rel_switches()
+			open_rel_doors()
+			group_is_on = true
+	else:
+		turn_on_rel_switches()
+		open_rel_doors()
+		group_is_on = true
+
+
+func on_switch_turned_off():
+	close_rel_doors()
+	turn_off_rel_switches()
+	group_is_on = false
