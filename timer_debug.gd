a7c04081941a5ffc5299a4dc31640392f12bab21
caiolopezcomz <caiolopez@gmail.com>
2024-06-03 23:49:17 -0300

    added print option for timer debugger script

--- timer_debug.gd
+++ timer_debug.gd
@@ -1,13 +1,16 @@
 extends Timer
 
+@export var does_print: bool = false
 var was_stopped:= true
 var just_started:= false
 
 func _ready():
+	if not does_print: return
 	connect("timeout", on_timeout)
 
 
 func _process(_delta):
+	if not does_print: return
 	just_started = was_stopped and not is_stopped()
 	was_stopped = is_stopped()
 	if just_started:
@@ -15,4 +18,5 @@ func _process(_delta):
 
 
 func on_timeout():
+	if not does_print: return
 	print("TIMER ", name, " TIMEOUT")
