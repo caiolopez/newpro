9c72cf883655f7ebeb1127371f4c5a29bbc9acaf
caiolopezcomz <caiolopez@gmail.com>
2024-06-04 23:59:59 -0300

    cleanup and weird tmp files

--- Hero/hero_sprite.gd
+++ Hero/hero_sprite.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
 var original_material: Material = null
-var white_material: ShaderMaterial = preload("res://CaioShaders/white.tres")
 
 func _ready():
 	original_material = material
@@ -9,10 +8,4 @@ func _ready():
 
 func _process(_delta):
 	set_flip_h(get_parent().facing_direction == -1)
-
-
-func paint_white(active: bool):
-	if active: material = white_material
-	else: material = original_material
-	
 
