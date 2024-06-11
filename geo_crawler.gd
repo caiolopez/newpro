6a174f5af9c48eaca398566c88acaf2907f0c595
caiolopezcomz <caiolopez@gmail.com>
2024-06-11 14:17:17 -0300

    geocrawler snaps!

--- geo_crawler.gd
+++ geo_crawler.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
+@export var velocity: Vector2 = Vector2(500, 0)
 enum Direction {UP, DOWN, LEFT, RIGHT}
-var SPEED: float = 100.0
-var velocity: Vector2 = Vector2(50, 0)
 var was_col: bool = false
+var diff: Vector2
 
 func _ready():
 	$DownRC.force_raycast_update()
@@ -17,10 +17,11 @@ func update_direction():
 	if not changed:
 		return
 
-	# if $DownRC.is_colliding():
-	#	var diff = to_local($DownRC.get_collision_point()) - $DownRC.position TODO define raycast object
-
-
+	if $DownRC.is_colliding():
+		diff = to_local($DownRC.get_collision_point()) - $CollisionShape2D.shape.get_rect().size/2
+		move_local_y(diff.y)
+		print(diff)
+	
 	if not $DownRC.is_colliding():
 		self.rotation_degrees += 90
 		velocity = velocity.rotated(deg_to_rad(90))
