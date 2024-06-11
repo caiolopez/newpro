54081c41f994b038f83564319edce40fd133ad1d
caiolopezcomz <caiolopez@gmail.com>
2024-06-10 22:02:04 -0300

    change name of facing direction component

--- EntityComponents/face_hero.gd
+++ EntityComponents/face_hero.gd
@@ -1,7 +1,7 @@
-class_name FacingDirection extends Node2D
+class_name FaceHero extends Node2D
 
-@export var face_hero: bool = false ## If set to true, the entity will always flip horizontally to face the hero. If set to false, it will obbey movement components.
-@onready var parent: CharacterBody2D = get_parent()
+@export var face_hero: bool = false
+@onready var parent: Node2D = get_parent()
 @onready var hero: CharacterBody2D
 var last_hero_dir: int
 signal update(dir: int)
@@ -13,7 +13,7 @@ func _ready():
 
 
 func _process(_delta):
-	if face_hero and hero:
+	if hero:
 		var hero_dir = sign(hero.global_position.x - parent.global_position.x)
 		if hero_dir != last_hero_dir:
 			last_hero_dir = hero_dir
