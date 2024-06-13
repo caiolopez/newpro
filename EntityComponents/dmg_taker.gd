7931366b6e352feaedda69cf710765ac8c0a94db
caiolopezcomz <caiolopez@gmail.com>
2024-06-13 00:43:27 -0300

    Update dmg_taker.gd

--- EntityComponents/dmg_taker.gd
+++ EntityComponents/dmg_taker.gd
@@ -35,14 +35,15 @@ func _on_area_entered(area):
 	if current_hp <= 0: return
 	if not area.is_in_group("dmg_dealers"): return
 	if is_foe == area.is_foe: return
+	if not area.visible: return
 	
-	if area.is_in_group("bullets") and area.visible:
+	if area.is_in_group("bullets"):
 		if not area.is_fire and immune_to_regular_bullets:
 			return
 		if current_hp > 0:
 			area.kill_bullet()
 
-		take_dmg(area.DMG_AMOUNT)
+	take_dmg(area.DMG_AMOUNT)
 
 
 func commit_status():
