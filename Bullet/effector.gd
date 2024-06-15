1bc8052aae4f42dba75d4925dfb273d8cf1eaf26
caiolopezcomz <caiolopez@gmail.com>
2024-06-15 17:05:56 -0300

    effector now uses UniqueCollider

--- Bullet/effector.gd
+++ Bullet/effector.gd
@@ -3,7 +3,7 @@ class_name Effector extends Area2D
 enum Effects {QUEUE_FREE, DISPLACE, ENABLE, RESET_BEHAVIOR}
 @export var effect: Effects = Effects.QUEUE_FREE ## The effect that will happen to all children of this node.
 @export var destination: Vector2 ## If set to displace entities, this is the global position they will be instantly moved to.
-var entities: Array[Node]
+@export var entities: Array[Node] ## The entities that will be affected. Note: All children of Effector will automatically be included in this Array.
 
 
 func _ready():
@@ -14,20 +14,25 @@ func _ready():
 
 
 func on_body_entered(body):
-	if not body.is_in_group("heroes"): return
+	if not body is Hero: return
 	for e in entities:
 		if e != null:
 			match effect:
 				Effects.QUEUE_FREE: e.queue_free()
-				Effects.DISPLACE: e.global_position = destination
+				Effects.DISPLACE: displace_entities(e)
 				Effects.ENABLE: add_child(e)
 				Effects.RESET_BEHAVIOR: reset_resetibles(e)
 
 
 func add_children_to_entities():
 	for c in get_children():
-		if not c is CollisionShape2D:
+		if not c == $UniqueCollider:
 			entities.append(c)
+
+
+func displace_entities(e: Node):
+	if e is Node2D:
+		e.global_position = destination
 
 
 func disable_entities():
