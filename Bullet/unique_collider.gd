23fdc7a0a0f2cedc39562231a277bd3c41c222f3
caiolopezcomz <caiolopez@gmail.com>
2024-06-20 16:39:55 -0300

    attempting to improve unique collider

--- Bullet/unique_collider.gd
+++ Bullet/unique_collider.gd
@@ -1,8 +1,10 @@
 @tool
 class_name UniqueCollider extends CollisionShape2D
 
+var is_unique: bool = false
 
 func _ready():
-	if Engine.is_editor_hint() and shape:
+	if Engine.is_editor_hint() and shape and not is_unique:
 		shape = shape.duplicate()
+		is_unique = true
 		print("Generated unique shape for ", self.name)
