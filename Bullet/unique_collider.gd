c16b32f5fde86cf8b066d6ee5d6d1c035bf9eca7
caiolopezcomz <caiolopez@gmail.com>
2024-06-20 17:03:40 -0300

    unique collider done!

--- Bullet/unique_collider.gd
+++ Bullet/unique_collider.gd
@@ -1,7 +1,7 @@
 @tool
 class_name UniqueCollider extends CollisionShape2D
 
-var is_unique: bool = false
+@export var is_unique: bool = false ## Read only. This variable is only exported so it can be serialized. Do not change it.
 
 func _ready():
 	if Engine.is_editor_hint() and shape and not is_unique:
