6538f447d2f855613bdaf1ad07d436f966d13300
caiolopezcomz <caiolopez@gmail.com>
2024-06-26 21:08:25 -0300

    minimap wip

--- GUI/mini_map.gd
+++ GUI/mini_map.gd
@@ -6,19 +6,28 @@ var min_scale: Vector2:
 	get: return Vector2(minimum_scale, minimum_scale)
 var max_scale: Vector2:
 	get: return Vector2(maximum_scale, maximum_scale)
-@onready var mini_hero: Sprite2D = $MiniHero
+@onready var mini_hero: Sprite2D = $Icons/MiniHero
+var waypoint_scene = load("res://GUI/Waypoint.tscn")
 var default_scale: float = 0.1
-var scale_speed: Vector2 = Vector2(1, 1)
+var scale_speed: Vector2 = Vector2(2.5, 2.5)
 var move_speed: Vector2 = Vector2(400, 400)
 var map_flags: Array[Node2D] = []
-enum States {ON, ANIMATING, OFF}
+enum States {ON, ANIMATING_IN, ANIMATING_OUT, OFF}
 var state: States = States.OFF
+var boundaries: CollisionShape2D
 
 func _ready():
+	boundaries = find_boundaries().duplicate()
+	$SectorPolygons.add_child(boundaries)
 	Events.chart_map_sector.connect(append_sector_to_map)
 	Events.unchart_map_sector.connect(delete_sector_from_map)
+	$SectorPolygons.self_modulate = Color.TRANSPARENT
+	$Icons.self_modulate = Color.TRANSPARENT
 	adjust_icon_scale()
 	center_map()
+
+func find_boundaries() -> Node:
+	return get_node("/root/Level/Bounderies")
 
 func append_sector_to_map(sector: MapSector):
 	var sect_coll = sector.extract_coll_polygon_2d()
@@ -31,13 +40,14 @@ func append_sector_to_map(sector: MapSector):
 
 func delete_sector_from_map(sector: MapSector):
 	for poly in $SectorPolygons.get_children():
-		if poly.ref == sector:
+		if poly == SectorPolygon and poly.ref == sector:
 			poly.queue_free()
 
 func update_mini_hero_pos():
 	mini_hero.position = Utils.find_hero().global_position
 
 func center_map():
+	update_mini_hero_pos()
 	position = Vector2.ZERO - mini_hero.position * scale + get_viewport_rect().size / 2
 
 func adjust_scale(delta, direction):
@@ -50,31 +60,42 @@ func adjust_scale(delta, direction):
 
 func flag_minimap():
 	var new_flag
-	if map_flags.size() <= 1:
-		new_flag = $Waypoint.duplicate()
-		add_child(new_flag)
+	if map_flags.size() < 1:
+		new_flag = waypoint_scene.instantiate()
+		$Icons.add_child(new_flag)
 		map_flags.append(new_flag)
 	else:
 		new_flag = map_flags.pop_front()
 		map_flags.append(new_flag)
 	new_flag.position = (get_viewport_rect().size / 2 - position) / scale
-	for i in range(map_flags.size()):
-		map_flags[i].modulate = Color(1, 0, 1, i / float(map_flags.size()))
+	adjust_icon_scale()
+	# for i in range(map_flags.size()): map_flags[i].modulate = Color(1, 0, 1, i / float(map_flags.size()))
 
 func adjust_icon_scale():
-	for child in get_children():
-		if child.is_in_group("map_icons"):
-			child.scale = Vector2.ONE / scale
+	for child in $Icons.get_children():
+		child.scale = Vector2.ONE / scale
 
 func _process(delta):
 	if state == States.OFF\
 	and Input.is_action_just_pressed("pause"):
-		show_minimap()
+		center_map()
+		fade_in_minimap()
+		state = States.ANIMATING_IN
 		return
 	
-	if state == States.ANIMATING:
-		center_map()
-		adjust_icon_scale()
+	if state == States.ANIMATING_IN:
+		if scale > min_scale:
+			adjust_scale(delta, -1)
+		else:
+			state = States.ON
+		update_mini_hero_pos()
+		return
+	
+	if state == States.ANIMATING_OUT:
+		if scale < Vector2.ONE:
+			adjust_scale(delta, 1)
+		else:
+			state = States.OFF
 		update_mini_hero_pos()
 		return
 	
@@ -82,7 +103,9 @@ func _process(delta):
 		return
 
 	update_mini_hero_pos()
-	if Input.is_action_pressed("up"):    position.y -= move_speed.y * delta
+	if Input.is_action_pressed("up"):
+		position = flur(position, Vector2(0, -move_speed.y * delta))
+		#position.y = position.y - move_speed.y * delta
 	if Input.is_action_pressed("down"):  position.y += move_speed.y * delta
 	if Input.is_action_pressed("left"):  position.x -= move_speed.x * delta
 	if Input.is_action_pressed("right"): position.x += move_speed.x * delta
@@ -94,47 +117,24 @@ func _process(delta):
 	if Input.is_action_just_pressed("flag_minimap"): flag_minimap()
 	
 	if Input.is_action_just_pressed("unpause"):
-		#hide_icons()
-		hide_minimap()
+		fade_out_minimap()
+		state = States.ANIMATING_OUT
 
-func show_minimap():
-	state = States.ANIMATING
-	var tween = create_tween()
-	tween.tween_property(
-		self,
-		"scale",
-		min_scale,
-		0.5).set_ease(Tween.EaseType.EASE_IN)
-	tween.parallel().tween_property(
-		$SectorPolygons,
-		"self_modulate",
-		Color.WHITE,
-		0.5)
-	tween.tween_callback(func():
-		#show_icons()
-		state = States.ON)
+func flur(coordinate: Vector2, displacement: Vector2) -> Vector2:
+	var sum = coordinate + displacement
+	if boundaries.shape.get_rect().has_point(sum):
+		return sum
+	else:
+		return coordinate
 
-func hide_minimap():
-	state = States.ANIMATING
-	var tween = create_tween()
-	tween.tween_property(
-		self,
-		"scale",
-		Vector2.ONE,
-		0.5).set_ease(Tween.EaseType.EASE_IN)
-	tween.parallel().tween_property(
-		$SectorPolygons,
-		"self_modulate",
-		Color.TRANSPARENT,
-		0.5)
-	tween.tween_callback(func(): state = States.OFF)
+func fade_in_minimap():
+	var t = create_tween()
+	t.tween_property($SectorPolygons, "self_modulate", Color.WHITE, 0.5)
+	t.parallel().tween_property($Icons, "self_modulate", Color.WHITE, 0.5)
 
-func show_icons():
-	for child in get_children():
-		if child.is_in_group("map_icons"):
-			child.show()
+func fade_out_minimap():
+	var t = create_tween()
+	t.tween_property($SectorPolygons, "self_modulate", Color.TRANSPARENT, 0.25)
+	t.parallel().tween_property($Icons, "self_modulate", Color.TRANSPARENT, 0.25)
 
-func hide_icons():
-	for child in get_children():
-		if child.is_in_group("map_icons"):
-			child.hide()
+
---
+++ GUI/waypoint.gd
@@ -0,0 +1,1 @@
+class_name Waypoint extends Sprite2D
