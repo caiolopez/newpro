cbde2393de5f97cba0f3e08ab752dc31f935f193
caiolopezcomz <caiolopez@gmail.com>
2024-07-04 19:00:42 -0300

    deleted everything regarding minimap outlines

--- GUI/mini_map.gd
+++ GUI/mini_map.gd
@@ -17,9 +17,9 @@ var state: States = States.OFF
 
 func _ready():
 	Events.chart_map_sector.connect(append_sector_to_map)
+	Events.unchart_map_sector.connect(delete_sector_from_map)
 	$SectorPolygons.self_modulate = Color.TRANSPARENT
 	$Icons.self_modulate = Color.TRANSPARENT
-	$Outlines.self_modulate = Color.TRANSPARENT
 	adjust_icon_scale()
 	center_map()
 
@@ -31,57 +31,14 @@ func append_sector_to_map(sector: MapSector):
 	new_mini_sector.polygon = sect_coll.polygon
 	new_mini_sector.global_position = sect_coll.global_position
 	$SectorPolygons.add_child(new_mini_sector)
-	update_outline_node()
 
 func delete_sector_from_map(sector: MapSector):
 	for poly in $SectorPolygons.get_children():
 		if poly is SectorPolygon and poly.ref == sector:
 			poly.queue_free()
-	update_outline_node() # TODO : outlines are ignoring this
 
 func update_mini_hero_pos():
 	mini_hero.position = Utils.find_hero().global_position
-
-func update_outline_node():
-	for child in $Outlines.get_children():
-		child.queue_free()
-	
-	for polygon in merge_mini_sectors():
-		var outline: Line2D = Line2D.new()
-		outline.points = polygon
-		$Outlines.add_child(outline)
-		outline.closed = true
-		outline.default_color = Color.MIDNIGHT_BLUE
-		outline.width = 50
-		outline.joint_mode = Line2D.LINE_JOINT_ROUND
-
-func merge_mini_sectors() ->  Array[PackedVector2Array]:
-	var parent_node = $SectorPolygons
-	var polygons: Array[PackedVector2Array] = []
-	
-	for child in parent_node.get_children():
-		if child is Polygon2D:
-			polygons.append(get_translated_polygon(child))
-
-	if polygons.is_empty():
-		push_error("No polygons found to merge.")
-
-	var merged_polygons: Array[PackedVector2Array] = [polygons[0]]
-	for i in range(1, polygons.size()):
-		var other_polygons: Array[PackedVector2Array] = []
-		if merged_polygons.size() > 1:
-			other_polygons = merged_polygons.duplicate()
-			other_polygons.remove_at(0)
-		merged_polygons = Geometry2D.merge_polygons(merged_polygons[0], polygons[i])
-		merged_polygons.append_array(other_polygons)
-
-	return merged_polygons
-
-func get_translated_polygon(poly2d: Polygon2D) -> PackedVector2Array:
-	var result_polygon: PackedVector2Array = []
-	for point in poly2d.polygon:
-		result_polygon.append(point + poly2d.position)
-	return result_polygon
 
 func center_map():
 	update_mini_hero_pos()
@@ -92,7 +49,6 @@ func adjust_scale(delta, direction):
 	scale += direction * scale_speed * delta
 	var scale_factor = scale / old_scale
 	position = (position - get_viewport_rect().size / 2) * scale_factor + get_viewport_rect().size / 2
-	
 	adjust_icon_scale()
 
 func flag_minimap():
@@ -106,7 +62,6 @@ func flag_minimap():
 		map_flags.append(new_flag)
 	new_flag.position = (get_viewport_rect().size / 2 - position) / scale
 	adjust_icon_scale()
-	# for i in range(map_flags.size()): map_flags[i].modulate = Color(1, 0, 1, i / float(map_flags.size()))
 
 func adjust_icon_scale():
 	for child in $Icons.get_children():
@@ -159,10 +114,8 @@ func fade_in_minimap():
 	var t = create_tween()
 	t.tween_property($SectorPolygons, "self_modulate", Color.CORNFLOWER_BLUE, 0.5)
 	t.parallel().tween_property($Icons, "self_modulate", Color.WHITE, 0.5)
-	t.parallel().tween_property($Outlines, "self_modulate", Color.MIDNIGHT_BLUE, 0.5)
 
 func fade_out_minimap():
 	var t = create_tween()
 	t.tween_property($SectorPolygons, "self_modulate", Color.TRANSPARENT, 0.25)
 	t.parallel().tween_property($Icons, "self_modulate", Color.TRANSPARENT, 0.25)
-	t.parallel().tween_property($Outlines, "self_modulate", Color.TRANSPARENT, 0.25)
