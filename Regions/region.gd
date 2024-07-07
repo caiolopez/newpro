1bab6b7bf00069e38bc5689eb3cabbb96d5f9cb6
caiolopezcomz <caiolopez@gmail.com>
2024-07-07 01:26:17 -0300

    region manager now only loads a region from resource if the region is not currently on tree

--- Regions/region.gd
+++ Regions/region.gd
@@ -1,5 +1,1 @@
 class_name Region extends Node2D
-
-
-func _ready():
-	RegionManager.set_current_region(self)
--- Regions/region_manager.gd
+++ Regions/region_manager.gd
@@ -5,9 +5,17 @@ var current_region: Region = null
 func change_region(to_region: Constants.RegName) -> void:
 	if current_region:
 		current_region.queue_free()
-	var new_region = load("res://Regions/" + Constants.RegName.keys()[to_region] + ".tscn").instantiate()
-	new_region.name = Constants.RegName.keys()[to_region]
-	get_tree().root.add_child(new_region)
+		print("Region ", current_region.name, " freed.")
+
+	var new_region = get_node_or_null("/root/Level/" + Constants.RegName.keys()[to_region])
+
+	if not new_region:
+		new_region = load("res://Regions/" + Constants.RegName.keys()[to_region] + ".tscn").instantiate()
+		new_region.name = Constants.RegName.keys()[to_region]
+		get_node("/root/Level").call_deferred("add_child", new_region)
+		print("Region ", new_region.name, " loaded from resource.")
+
+	current_region = new_region
 
 func set_current_region(region: Region):
 	current_region = region
