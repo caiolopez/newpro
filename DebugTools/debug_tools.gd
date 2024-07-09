b215b65780de813366899ce3246e1bb1d768fdb1
caiolopezcomz <caiolopez@gmail.com>
2024-07-09 14:59:16 -0300

    debug tools parameter change

--- DebugTools/debug_tools.gd
+++ DebugTools/debug_tools.gd
@@ -9,18 +9,13 @@ func _process(_delta):
 
 func input_bin():
 	if not input_stuff: return
-	if Input.is_action_just_pressed("shoulder_left"): RegionManager.change_region(Constants.RegName.HUB)
-	if Input.is_action_just_pressed("shoulder_right"):
-		SaveManager.load_from_slot(SaveManager.current_slot)
+	if Input.is_action_just_pressed("shoulder_left"):
+		RegionManager.change_region(Constants.RegName.HUB)
 	if Input.is_action_just_pressed("Debug Action 1"):
 		SaveManager.print_all_dics()
 	if Input.is_action_just_pressed("Debug Action 2"):
-		print(Utils.parse_time_as_string(AppManager.game_time))
-	if Input.is_action_just_pressed("up"): SaveManager.save_file()
-	if Input.is_action_just_pressed("center_map"): SaveManager.load_file()
-	if Input.is_action_just_pressed("pause"):
-		SaveManager.inject_changes_into_current_region()
-		SaveManager.inject_changes_into_hero()
+		print(Utils.parse_time_as_string(AppManager.game_time, false))
+
 
 func print_bin():
 	if not print_stuff: return
