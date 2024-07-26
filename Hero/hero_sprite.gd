618134b1b73dd37593387ccaea36c09d93841085
caiolopezcomz <caiolopez@gmail.com>
2024-07-26 00:01:00 -0300

    hero horizontal flip is now done using scale

--- Hero/hero_sprite.gd
+++ Hero/hero_sprite.gd
@@ -1,19 +1,14 @@
 extends Node2D
 
 var original_material: Material = null
-var flippable_children = {}
+@onready var original_scale: Vector2 = scale
 @onready var hero: = get_parent()
 
 func _ready():
 	material.set_shader_parameter("replace_black", Constants.HERO_DARK)
 	material.set_shader_parameter("replace_white", Constants.HERO_LIGHT)
 	original_material = material
-	for child in get_children():
-		if child.has_method("set_flip_h"):
-			flippable_children[child] = child.position
 
 func _process(_delta):
-	for child in flippable_children:
-		child.set_flip_h(hero.facing_direction == -1)
-		child.rotation_degrees = hero.current_blunder_jump_angle
-		child.position.x = flippable_children[child].x * hero.facing_direction
+		rotation_degrees = hero.current_blunder_jump_angle
+		scale.x = original_scale.x * hero.facing_direction
