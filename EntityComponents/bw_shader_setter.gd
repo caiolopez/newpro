e232c052e21dcfdca4eb792ed67b570875a967a7
caiolopezcomz <caiolopez@gmail.com>
2024-07-30 22:08:46 -0300

    bw_shader_setter now accepts a constant as a color

--- EntityComponents/bw_shader_setter.gd
+++ EntityComponents/bw_shader_setter.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-@export var dark_color: Color = Color.BLACK
-@export var light_color: Color = Color.WHITE
+@export var dark_color: Color = Color.BLACK ## The color that will replace the black pixels. Note: Only works as a fallback if the dark_color_name field is empty or invalid.
+@export var dark_color_name: StringName ## The name of the constant bearing the desired color value, as implemented in the Constants file. This has priority over the color picker value.
+@export var light_color: Color = Color.WHITE ## The color that will replace the white pixels. Note: Only works as a fallback if the light_color_name field is empty or invalid.
+@export var light_color_name: StringName ## The name of the constant bearing the desired color value, as implemented in the Constants file. This has priority over the color picker value.
+
 @onready var parent: Node2D = get_parent()
 var mat: ShaderMaterial
 
@@ -9,7 +12,11 @@ var mat: ShaderMaterial
 func _ready() -> void:
 	mat = ShaderMaterial.new()
 	mat.shader = load("res://CaioShaders/bw_replacer.gdshader")
+	if Constants.get(dark_color_name):
+		dark_color = Constants.get(dark_color_name)
 	mat.set_shader_parameter("replace_black", dark_color)
+	if Constants.get(light_color_name):
+		light_color = Constants.get(light_color_name)
 	mat.set_shader_parameter("replace_white", light_color)
-	parent.material = material
+	parent.material = mat
 	
