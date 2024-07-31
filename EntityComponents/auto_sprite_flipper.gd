71c77c8b6bfdd04261b809bd3f029820db91e0ae
caiolopezcomz <caiolopez@gmail.com>
2024-07-30 22:07:25 -0300

    auto sprite flipper component

---
+++ EntityComponents/auto_sprite_flipper.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var randomize_h: bool = true
+@export var randomize_v: bool = false
+@onready var parent: Node2D = get_parent()
+
+func _ready() -> void:
+	randomize_flip()
+	parent.animation_finished.connect(randomize_flip)
+
+func randomize_flip():
+	if randomize_h:
+		parent.flip_h = bool(randi() % 2)
+	if randomize_v:
+		parent.flip_v = bool(randi() % 2)
