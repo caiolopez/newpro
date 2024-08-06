877a6d38db4a02a2292a83f7edf77ce288ee417f
caiolopezcomz <caiolopez@gmail.com>
2024-08-05 22:57:18 -0300

    better bullet_dies animation

--- EntityComponents/auto_sprite_flipper.gd
+++ EntityComponents/auto_sprite_flipper.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var randomize_h: bool = true
 @export var randomize_v: bool = false
+@export var randomize_90deg_turn: bool = false
 @onready var parent: Node2D = get_parent()
 
 func _ready() -> void:
@@ -13,3 +14,5 @@ func randomize_flip():
 		parent.flip_h = bool(randi() % 2)
 	if randomize_v:
 		parent.flip_v = bool(randi() % 2)
+	if randomize_90deg_turn:
+		parent.rotation_degrees = 90
