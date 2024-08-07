56a920151fccfe2b0a12f6885f0dd67545f62137
caiolopezcomz <caiolopez@gmail.com>
2024-08-07 00:17:15 -0300

    changed the double-trigger workaround: instead of visibility, we now compare the instance name

--- EntityComponents/dmg_taker.gd
+++ EntityComponents/dmg_taker.gd
@@ -7,6 +7,7 @@ class_name DmgTaker extends Area2D
 @onready var current_hp: int = HP_AMOUNT
 var immune: bool = false
 var immune_to_regular_bullets: bool = false ## Will not take damage from non-incendiary bullets.
+var last_processed_bullet: StringName = &""
 
 signal died
 signal resurrected
@@ -16,6 +17,8 @@ func _ready():
 	Events.hero_reached_checkpoint.connect(commit_status)
 	Events.hero_respawned_at_checkpoint.connect(reset_status)
 	area_entered.connect(on_area_entered)
+	area_exited.connect(func(area): if area.name == last_processed_bullet:
+		last_processed_bullet = &"", CONNECT_DEFERRED)
 
 
 func take_dmg(amount: int):
@@ -36,11 +39,11 @@ func on_area_entered(area):
 	if current_hp <= 0: return
 	if not area.is_in_group("dmg_dealers"): return
 	if is_foe == area.is_foe: return
-	if not area.visible: return
 	
 	if area is Bullet:
-		if not area.is_fire and immune_to_regular_bullets:
-			return
+		if area.name == last_processed_bullet: return
+		last_processed_bullet = area.name
+		if not area.is_fire and immune_to_regular_bullets: return
 		if current_hp > 0:
 			area.kill_bullet()
 
--- SwitchesAndDoors/switch.gd
+++ SwitchesAndDoors/switch.gd
@@ -3,13 +3,15 @@ class_name Switch extends Area2D
 @export var toggle: bool = false ## Default switches (toggle == false) can only be turned on. Set this to true if this switch can be turned off as well.
 var controller: Node2D
 var current_state: Constants.SwitchState
-
+var last_processed_bullet: StringName = &""
 
 func _ready():
 	controller = get_parent()
 	$TimerSimultWindow.set_wait_time(controller.simult_window_duration)
 	$TimerSimultWindow.timeout.connect(on_simult_window_timeout)
 	area_entered.connect(on_bullet_entered)
+	area_exited.connect(func(area): if area.name == last_processed_bullet:
+		last_processed_bullet = &"", CONNECT_DEFERRED)
 	switch_off()
 
 
@@ -37,7 +39,9 @@ func on_simult_window_timeout():
 
 func on_bullet_entered(area):
 	if not area is Bullet: return
-	if not area.visible: return
+	if area.name == last_processed_bullet: return
+
+	last_processed_bullet = area.name
 
 	match current_state:
 		Constants.SwitchState.OFF:
@@ -51,3 +55,7 @@ func on_bullet_entered(area):
 			$TimerSimultWindow.start()
 		
 	area.kill_bullet()
+
+func on_bullet_exited(area):
+	if area.name == last_processed_bullet:
+		last_processed_bullet = &""
