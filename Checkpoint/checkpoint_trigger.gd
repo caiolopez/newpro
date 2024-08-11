69d7acbd30ba9ae1a350566b0e49b9bb1b08864d
caiolopezcomz <caiolopez@gmail.com>
2024-08-11 01:07:21 -0300

    added checkpoint functionality

--- Checkpoint/checkpoint_trigger.gd
+++ Checkpoint/checkpoint_trigger.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@export var recall_hero_direction: bool = false
+@export var recall_hero_direction: bool = false ## If set to false, respawns the hero facing forward (use negative x-values to flip trigger and hero spawn direction). If set to true, will respawn the hero facing the direction he was when the trigger was entered.
+@export var force_trigger: bool = false ## If set to true, causes this checkpoint to work even if it is already the current one. NOTE: Developed so designers don't have to place two normal checkpoints back to back after boss battles.
 var direction: int = 1
 
 func _ready():
@@ -9,7 +10,8 @@ func _ready():
 
 func on_body_entered(body):
 	if not body is Hero: return
-	if body.current_checkpoint_path == self.get_path(): return
+	if body.current_checkpoint_path == self.get_path() and not force_trigger: return
 	if recall_hero_direction: direction = body.facing_direction
 	body.update_current_checkpoint_path(self.get_path())
 	Events.hero_reached_checkpoint.emit()
+ 
