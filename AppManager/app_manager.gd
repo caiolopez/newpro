10310a8a53e05a89272cf08d287d0ce51eec0839
caiolopezcomz <caiolopez@gmail.com>
2024-08-20 23:53:35 -0300

    using app_state

--- AppManager/app_manager.gd
+++ AppManager/app_manager.gd
@@ -1,8 +1,12 @@
 extends Node
 
+@onready var state_machine: StateMachine = $StateMachine
 var game_time: float = 0.0
 var is_time_running: bool = false
 
+func _ready() -> void:
+	state_machine.start()
+
 func _process(delta):
-	if is_time_running and not get_tree().paused:
+	if is_time_running and not state_machine.is_current_state("AppStatePaused"):
 		game_time += delta
