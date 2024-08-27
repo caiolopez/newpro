f610562e9b5ba8f679aa0776d771969570142baf
caiolopezcomz <caiolopez@gmail.com>
2024-08-26 21:44:10 -0300

    dmg_taker now has a list of bullet_types it's immune to

--- EntityComponents/dmg_taker.gd
+++ EntityComponents/dmg_taker.gd
@@ -3,10 +3,10 @@ class_name DmgTaker extends Area2D
 @export var HP_AMOUNT: int = 1
 @export var RESET_UPON_RESPAWN: bool = true ## When set to true, entities will have all their attributes reset when the Hero respawns.
 @export var QUEUE_FREE_ON_CHECKPOINT: bool = true ## When set to true, will prevent dead entities from reappearing when the Hero respawns.
+@export var immune_to: Array[Constants.BulletType] = [] ## Bullet types in this list will not affect entities.
 @onready var is_foe: bool = Utils.check_if_foe(self.get_parent()) ## If no FriendOrFoe sibling component is found, assumes is_foe = true.
 @onready var current_hp: int = HP_AMOUNT
-var immune: bool = false
-var immune_to_regular_bullets: bool = false ## Will not take damage from non-incendiary bullets.
+var currently_immune: bool = false
 var last_processed_bullet: Bullet = null
 
 signal died
@@ -22,7 +22,7 @@ func _ready():
 
 
 func take_dmg(amount: int):
-	if immune\
+	if currently_immune\
 	or current_hp <= 0:
 		return
 
@@ -38,14 +38,13 @@ func take_dmg(amount: int):
 func on_area_entered(area):
 	if current_hp <= 0: return
 	if not area.is_in_group("dmg_dealers"): return
-	if is_foe == area.is_foe: return
-	
+	if area.is_foe == self.is_foe: return
+
 	if area is Bullet:
 		if area == last_processed_bullet: return
 		last_processed_bullet = area
-		if not area.bullet_type == Constants.BulletType.FIRE and immune_to_regular_bullets: return
-		if current_hp > 0:
-			area.kill_bullet()
+		area.kill_bullet()
+		if area.bullet_type in immune_to: return
 
 	take_dmg(area.DMG_AMOUNT)
 
