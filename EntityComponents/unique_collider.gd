95e93c7127c711e353eeebfee9de4d64a7bc54a0
caiolopezcomz <caiolopez@gmail.com>
2024-09-10 00:08:34 -0300

    unique colider changes, but still not great.

--- EntityComponents/unique_collider.gd
+++ EntityComponents/unique_collider.gd
@@ -1,10 +1,20 @@
 @tool
 class_name UniqueCollider extends CollisionShape2D
 
-@export var is_unique: bool = false ## Read only. This variable is only exported so it can be serialized. Do not change it.
+func _enter_tree():
+	if Engine.is_editor_hint():
+		call_deferred("ensure_unique_shape")
 
-func _ready():
-	if Engine.is_editor_hint() and shape and not is_unique:
-		shape = shape.duplicate()
-		is_unique = true
-		if DebugTools.print_stuff: print("Generated unique shape for ", self.name)
+func ensure_unique_shape():
+	if shape:
+		var current_hash = hash_shape(shape)
+		if not has_meta("original_shape_hash"):
+			set_meta("original_shape_hash", current_hash)
+		
+		if get_meta("original_shape_hash") == current_hash:
+			shape = shape.duplicate()
+			set_meta("original_shape_hash", hash_shape(shape))
+			if DebugTools.print_stuff: print("Generated unique shape for ", self.name)
+
+func hash_shape(s: Shape2D) -> int:
+	return hash(s.get_instance_id()) & 0x7FFFFFFF
