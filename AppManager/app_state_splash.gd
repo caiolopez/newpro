58e32a642182e7f4dd4abe635f78385ef0dda00e
caiolopezcomz <caiolopez@gmail.com>
2024-09-10 00:10:03 -0300

    Splash Reel (wip)

--- AppManager/app_state_splash.gd
+++ AppManager/app_state_splash.gd
@@ -1,7 +1,15 @@
 extends AppState
 
 func on_enter():
-	machine.set_state("AppStateInGame")
+	$"../../SplashReel".reset()
+	$"../../SplashReel".start_reel()
+	$"../../SplashReel".reel_finished.connect(func():
+		machine.set_state("AppStateInGame"),
+		CONNECT_ONE_SHOT)
+
+func on_process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		$"../../SplashReel".skip_all()
 
 func on_exit():
 	pass
---
+++ AppManager/splash_unit.gd
@@ -0,0 +1,5 @@
+class_name SplashUnit extends Control
+
+@export var fade_in_time: float = 1
+@export var sustain_time: float = 1.5
+@export var fade_out_time: float = 0.2
