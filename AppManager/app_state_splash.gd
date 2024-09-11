3a45699e069343d5c42f1485cdbb65a354a624ab
caiolopezcomz <caiolopez@gmail.com>
2024-09-11 00:52:48 -0300

    splash slide reel moved to singleton Hud

--- AppManager/app_state_splash.gd
+++ AppManager/app_state_splash.gd
@@ -1,16 +1,18 @@
 extends AppState
 
+@onready var reel = Hud.get_node("SlideReel")
+
 func on_enter():
-	$"../../SlideReel".reset()
-	$"../../SlideReel".start_reel()
-	$"../../SlideReel".reel_finished.connect(func():
+	reel.reset()
+	reel.start_reel()
+	reel.reel_finished.connect(func():
 		Events.curtain_fade_out.emit()
 		machine.set_state("AppStateInGame"),
 		CONNECT_ONE_SHOT)
 
 func on_process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		$"../../SlideReel".skip_all()
+		reel.skip_all()
 
 func on_exit():
 	pass
