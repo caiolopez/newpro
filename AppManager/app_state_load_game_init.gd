7e675097097cf60e2d41a24440bfaeefb8aedab0
caiolopezcomz <caiolopez@gmail.com>
2024-09-12 17:58:22 -0300

    Game will only go to AppStateInGame when load_from_slot is completed

--- AppManager/app_state_load_game_init.gd
+++ AppManager/app_state_load_game_init.gd
@@ -4,9 +4,10 @@ func on_enter():
 	var hud = load("res://GUI/Hud.tscn").instantiate()
 	UI.add_child(hud)
 	await get_tree().process_frame
-	SaveManager.load_from_slot()
-	await get_tree().process_frame
-	machine.set_state("AppStateInGame")
+	if SaveManager.load_from_slot():
+		machine.set_state("AppStateInGame")
+	else:
+		machine.set_state("AppStateSplash")
 
 func on_exit():
 	Events.curtain_fade_out.emit()
