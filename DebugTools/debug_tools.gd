8afbe51d4b8ec637de55633444c306380ec9fcba
caiolopezcomz <caiolopez@gmail.com>
2024-09-13 19:58:04 -0300

    combo cleanup

--- DebugTools/debug_tools.gd
+++ DebugTools/debug_tools.gd
@@ -2,20 +2,9 @@ extends Node2D
 
 @export var print_stuff: bool = true
 @export var input_stuff: bool = true
-@export var blunder_on_single_button: bool = false
 
 func _ready():
-	ComboParser.combo_performed.connect(func(combo):
-		if combo == "FullScreen": toggle_fullscreen())
-	ComboParser.combo_performed.connect(func(combo):
-		blunder_on_single_button = combo == "BlunderOnSingleButton")
-	ComboParser.combo_performed.connect(func(combo): if combo == "AllPowerups":
-		var hero = Utils.find_hero()
-		hero.handle_powerups("INCENDIARY_AMMO")
-		hero.handle_powerups("UNDERWATER_AMMO")
-		hero.handle_powerups("AQUALUNG")
-		hero.handle_powerups("TELEPORTER")
-		)
+	pass
 
 func _process(_delta):
 	input_bin()
@@ -40,10 +29,3 @@ func input_bin():
 
 func print_bin():
 	if not print_stuff: return
-
-func toggle_fullscreen():
-		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, false)
-		else:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
