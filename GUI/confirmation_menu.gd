fc098814b486b59206bcff778951c2f76213b719
caiolopezcomz <caiolopez@gmail.com>
2024-09-18 00:17:53 -0300

    confirmation menu component

---
+++ GUI/confirmation_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal confirmation_made(confirmed: bool)
+
+func _ready():
+	hide()
+	$VBoxContainer/YesButton.pressed.connect(func(): _on_decision_made(true))
+	$VBoxContainer/CancelButton.pressed.connect(func(): _on_decision_made(false))
+
+func _unhandled_input(event):
+	if visible and (event.is_action_pressed("ui_cancel")):
+		_on_decision_made(false)
+
+func ask_for_confirmation(label_text: String = "Are you sure?", button_text: String = "Yes") -> bool:
+	$VBoxContainer/SureLabel.text = label_text
+	$VBoxContainer/YesButton.text = button_text
+	show()
+	$VBoxContainer/CancelButton.grab_focus()
+	
+	var result = await confirmation_made
+	hide()
+	return result
+
+func _on_decision_made(decision: bool):
+	confirmation_made.emit(decision)
