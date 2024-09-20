0001b1d341b5448e301958ce23fba58c47d933c9
caiolopezcomz <caiolopez@gmail.com>
2024-09-20 19:07:20 -0300

    Fixed water tile surface art drawing, was inconsistent

--- Water/water.gd
+++ Water/water.gd
@@ -12,37 +12,20 @@ func _ready() -> void:
 			bw_shader_setter = child
 	
 	if tile_map_layer and collider:
-		setup_water_surface()
+		_setup_water_surface_art()
 
 func get_surface_global_position() -> float:
 	var shape_extents = collider.shape.extents
 	return collider.global_position.y - shape_extents.y
 
-func setup_water_surface() -> void:
-	var tile_map_scale = tile_map_layer.scale.x
-	if not tile_map_layer.tile_set:
-		printerr("TileSet not set for water surface TileMapLayer")
-		return
-
-	# Apply scale to TileMapLayer
-	tile_map_layer.scale = Vector2(tile_map_scale, tile_map_scale)
-
-	# Calculate the number of tiles needed
+func _setup_water_surface_art() -> void:
+	var fine_tune: Vector2 = Vector2(0, -80)
+	
 	var shape_extents = collider.shape.extents
-	var tile_size = tile_map_layer.tile_set.tile_size * tile_map_scale
-	var num_tiles = ceil(shape_extents.x * 2 / tile_size.x)
-
-	# Calculate the starting position
-	var start_pos = Vector2(
-		collider.position.x - shape_extents.x,
-		collider.position.y - shape_extents.y
-	)
-	var map_start_pos = tile_map_layer.local_to_map(start_pos / tile_map_scale)
-
-	# Place the animated water surface tiles
+	var tile_size = tile_map_layer.tile_set.tile_size.x * tile_map_layer.scale.x
+	var num_tiles = ceil(shape_extents.x * 2 / tile_size)
+	tile_map_layer.position = collider.position - shape_extents + fine_tune
 	for i in range(num_tiles):
-		var tile_pos = Vector2i(map_start_pos.x + i, map_start_pos.y)
-		tile_map_layer.set_cell(tile_pos, 0, Vector2i(0, 0), 0)
-
-	# Update the TileMapLayer
+		var tile_pos = Vector2i(i, 0)
+		tile_map_layer.set_cell(tile_pos, 0, Vector2i.ZERO, 0)
 	tile_map_layer.update_internals()
