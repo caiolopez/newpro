1b193bb0a38abc37d2b763db066d1e36bf9717d2
caiolopezcomz <caiolopez@gmail.com>
2024-10-22 18:21:18 -0300

    boss trigger and open doors if dead now take switch groups instead of only loose doors

--- Boss/boss_trigger.gd
+++ Boss/boss_trigger.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @export var boss: Node
-@export var doors_to_close: Array[Door] = []
+@export var doors_to_close: Array[Door] = [] ## Optional list of simple doors to close upon reaching trigger.
+@export var switch_group_to_close: SwitchGroupController = null ## Optional SwitchGroupController to close upon triggering.
 var is_active: bool = false
 
 func _ready():
@@ -11,13 +12,18 @@ func _ready():
 			return
 		if boss and boss != null:
 			Events.boss_trigger_entered.emit(boss)
-			close_doors()
+			_close_doors()
+			_close_switch_group()
 			is_active = true
 		)
 	Events.hero_respawned_at_checkpoint.connect(func():
 		is_active = false
 		)
 
-func close_doors():
+func _close_doors():
 	for d in doors_to_close:
 		d.close()
+
+func _close_switch_group():
+	if switch_group_to_close:
+		switch_group_to_close.close_whole_group()
--- EntityComponents/open_doors_if_dead.gd
+++ EntityComponents/open_doors_if_dead.gd
@@ -1,17 +1,21 @@
 class_name OpenDoorsIfDead extends Node
 
-@export var doors_to_open: Array[Door] = []
+@export var doors_to_open: Array[Door] = [] ## Optional list of simple doors to open when parent entity is dead.
+@export var switch_group_to_open: SwitchGroupController = null ## Optional switch group to open when parent is dead.
 @onready var dmg_taker: DmgTaker = Utils.find_dmg_taker(self.get_parent())
-
 
 func _ready():
 	if dmg_taker != null:
 		dmg_taker.died.connect(func():
 			if AppManager.hero.is_currently_dead(): return
 			for d in doors_to_open:
-				d.open()
+				if d: d.open()
+			if switch_group_to_open:
+				switch_group_to_open.open_whole_group()
 			)
 		dmg_taker.resurrected.connect(func():
 			for d in doors_to_open:
-				d.insta_close()
+				if d: d.insta_close()
+			if switch_group_to_open:
+				switch_group_to_open.close_whole_group(SwitchGroupController.INSTANTLY)
 			)
