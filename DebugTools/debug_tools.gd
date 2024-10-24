18b96758f8a1fe4694c566e9e4b09c880bed9051
caiolopezcomz <caiolopez@gmail.com>
2024-10-23 21:27:19 -0300

    Update debug_tools.gd

--- DebugTools/debug_tools.gd
+++ DebugTools/debug_tools.gd
@@ -19,6 +19,9 @@ func input_bin():
 	if DebugTools.debug_mode\
 	and Input.is_action_just_pressed("Debug Action 2"):
 		AppManager.hero.state_machine.set_state("StateDebug")
+	
+	if Input.is_action_just_pressed("shoot"): AudioManager.play_sound("res://AudioManager/shot.mp3")
+	#if Input.is_action_just_pressed("jump"): AudioManager.play_music("giorgio")
 
 	#if Input.is_action_just_pressed("shoot"): print("SHOOT!")
 	#if Input.is_action_just_pressed("jump"): print("J ", AppManager.hero.pelvis_back_rc.is_colliding(), AppManager.hero.shoulder_back_rc.is_colliding())
