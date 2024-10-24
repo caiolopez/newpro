3144f9edcefe00709463f053fc1961c553fb9f34
caiolopezcomz <caiolopez@gmail.com>
2024-10-23 21:27:39 -0300

    UPPERCASED constant name

--- Hero/state_wall_grabbing.gd
+++ Hero/state_wall_grabbing.gd
@@ -5,7 +5,7 @@ var death_prone: bool = true
 
 var sticky_offset: Vector2
 var sticky: Node2D = null
-const inverted: bool = true
+const INVERTED: bool = true
 
 func on_enter():
 	hero.velocity.y = 0
@@ -22,7 +22,7 @@ func on_enter():
 		sticky_offset = Vector2(hero.position.x - c.position.x, hero.position.y - c.position.y)
 
 func on_process(_delta: float):
-	hero.step_shooting(inverted)
+	hero.step_shooting(INVERTED)
 
 	if hero.is_on_floor():
 		machine.set_state("StateIdle")
