4b0b3f5bc6e790b153d3eb23ab5577ac62d0b8fd
caiolopezcomz <caiolopez@gmail.com>
2024-10-23 22:24:22 -0300

    added music triggers

---
+++ AudioManager/music_trigger.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export_enum("nothing", "giorgio", "gorgo") var track: String
+
+func _ready():
+	body_entered.connect(func(body):
+		if not body is Hero: return
+		if track == "nothing": AudioManager.stop_music()
+		else: AudioManager.play_music(track)
+		)
