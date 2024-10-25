b06eb1656062dd3df861ee9f6f181638ad82ec24
caiolopezcomz <caiolopez@gmail.com>
2024-10-24 22:39:31 -0300

    Update music_trigger.gd

--- AudioManager/music_trigger.gd
+++ AudioManager/music_trigger.gd
@@ -5,6 +5,7 @@ extends Area2D
 func _ready():
 	body_entered.connect(func(body):
 		if not body is Hero: return
+		if not body.state_machine.current_state.death_prone: return
 		if not track or track == "nothing": AudioManager.stop_music()
 		else: AudioManager.play_music(track)
 		)
