18ba821bba3c388ac5b40e354d06cf6bdd569f57
caiolopezcomz <caiolopez@gmail.com>
2024-10-24 22:36:54 -0300

    added  background3dtrigger

---
+++ 3DBackground/background_3d_trigger.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var background: StringName = ""
+
+func _ready():
+	body_entered.connect(func(body):
+		if not body is Hero: return
+		if not body.state_machine.current_state.death_prone: return
+		Background3DManager.change_background(background)
+		)
