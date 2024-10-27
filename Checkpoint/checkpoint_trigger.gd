dc82bd2b40965d458866d8dd9b5780b3f5664c7e
caiolopezcomz <caiolopez@gmail.com>
2024-10-27 01:32:59 -0300

    better usability to define checkpoint facing direction. Still sucks because its not serializing

--- Checkpoint/checkpoint_trigger.gd
+++ Checkpoint/checkpoint_trigger.gd
@@ -1,20 +1,26 @@
 extends Area2D
 
-@export var recall_hero_direction: bool = false ## If set to false, respawns the hero facing forward (use negative x-values to flip trigger and hero spawn direction). If set to true, will respawn the hero facing the direction he was when the trigger was entered.
+enum Direction {
+	LEFT = -1, ## Hero will respawn facing left.
+	RIGHT = 1, ## Hero will respawn facing right.
+	RECALL_HERO_DIRECTION = 0 ## Hero will respawn facing the direction he was when the trigger was entered.
+	}
+@export var direction: Direction = Direction.RECALL_HERO_DIRECTION
 @export var force_trigger: bool = false ## If set to true, causes this checkpoint to work even if it is already the current one. NOTE: Developed so designers don't have to place two normal checkpoints back to back after boss battles.
-var direction: int = 1
+var recall: bool = false
 
 func _ready():
+	if not direction:
+		recall = true
+		direction = Direction.RIGHT
 	body_entered.connect(_on_body_entered)
-	if scale.x < 0: direction = -1
 
 func _on_body_entered(body):
-	print("oi")
 	if not body is Hero: return
 	if not body.state_machine.current_state.death_prone: return
 	if body.current_checkpoint_path == self.get_path() and not force_trigger:
 		return
-	if recall_hero_direction:
+	if recall:
 		direction = body.facing_direction
 	body.update_current_checkpoint_path(self.get_path())
 	Events.hero_reached_checkpoint.emit()
