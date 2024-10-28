b966c4dd2aa773bb47ead62acfc839d12baa4895
caiolopezcomz <caiolopez@gmail.com>
2024-10-28 18:16:58 -0300

    checkpoint triggers are now individual shapes

--- Checkpoint/checkpoint_trigger.gd
+++ Checkpoint/checkpoint_trigger.gd
@@ -1,25 +1,25 @@
 extends Area2D
 
-enum Direction {
-	LEFT = -1, ## Hero will respawn facing left.
-	RIGHT = 1, ## Hero will respawn facing right.
-	RECALL_HERO_DIRECTION = 0 ## Hero will respawn facing the direction he was when the trigger was entered.
-	}
-@export var direction: Direction = Direction.RECALL_HERO_DIRECTION
-@export var always_trigger: bool = false ## If set to true, causes this checkpoint to work consecutively. When set to false (normal behavior), this checkpoint cannot be triggered multiple times in a row. NOTE: Developed so designers don't have to place two normal checkpoints back to back after boss battles.
-
 func _ready():
-	body_entered.connect(_on_body_entered)
+	body_shape_entered.connect(_on_body_shape_entered)
 
-func _on_body_entered(body):
-	if not body is Hero: return
-	if not body.state_machine.current_state.death_prone: return
-	if body.current_checkpoint_path == self.get_path() and not always_trigger:
+func _on_body_shape_entered(_rid, body, _body_shape, area_shape_index):
+	if not body is Hero and not body.state_machine.current_state.death_prone:
+		return
+	var local_shape_node = get_collision_shape_by_index(area_shape_index)
+	var direction: int = local_shape_node.direction
+	if body.current_checkpoint_path == local_shape_node.get_path() and not local_shape_node.always_trigger:
 		return
 	if not direction:
 		direction = body.facing_direction
 		
-	body.update_current_checkpoint_info(self.get_path(), direction)
+	body.update_current_checkpoint_info(local_shape_node.get_path(), direction)
 	Events.hero_reached_checkpoint.emit()
 	await get_tree().process_frame
 	SaveManager.save_file()
+
+func get_collision_shape_by_index(shape_index: int) -> CollisionShape2D:
+	var shapes = get_children().filter(func(node): return node is CollisionShape2D)
+	if shape_index >= 0 and shape_index < shapes.size():
+		return shapes[shape_index]
+	return null
