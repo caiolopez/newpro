65f5324e274480e0590e3c9da8537f801c590826
caiolopezcomz <caiolopez@gmail.com>
2024-11-11 00:22:12 -0300

    face hero now only evaluates hero when hero is ready

--- EntityComponents/face_hero.gd
+++ EntityComponents/face_hero.gd
@@ -2,10 +2,14 @@ class_name FaceHero extends Node
 
 @export var face_hero: bool = false
 @onready var parent: Node2D = get_parent()
-@onready var hero: Hero = AppManager.hero
-var last_hero_dir: int
+var hero: Hero = null
+var last_hero_dir: int = 1
 signal update(dir: int)
 
+func _ready() -> void:
+	AppManager.hero_ready.connect(func():
+		hero = AppManager.hero
+		)
 
 func _process(_delta):
 	if hero and face_hero:
