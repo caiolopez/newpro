a8e2a33dbdb1fa6aca58da7eb4bd5ef8bb2aad78
caiolopezcomz <caiolopez@gmail.com>
2024-11-19 14:27:11 -0300

    hud management when saving and loading games

--- GUI/boss_overlay.gd
+++ GUI/boss_overlay.gd
@@ -12,13 +12,13 @@ func _ready():
 	hide()
 	Events.boss_trigger_entered.connect(_setup_bar)
 	Events.boss_trigger_entered.connect(func(_boss): _show_hp_bar())
-	Events.hero_died.connect(_hide_hp_bar)
+	Events.hero_died.connect(hide_hp_bar)
 
 func _setup_bar(boss):
 	current_boss = boss
 	$HpBar.max_value = dt.HP_AMOUNT
 	_update_bar()
-	dt.died.connect(_hide_hp_bar)
+	dt.died.connect(hide_hp_bar)
 	dt.suffered.connect(_update_bar_animated)
 	dt.resurrected.connect(_update_bar)
 
@@ -35,7 +35,7 @@ func _show_hp_bar():
 		.set_ease(Tween.EaseType.EASE_OUT)\
 		.set_delay(2)
 
-func _hide_hp_bar():
+func hide_hp_bar():
 	if not visible: return
 	var tween = create_tween()
 	tween.tween_property(
@@ -47,8 +47,8 @@ func _hide_hp_bar():
 		.set_ease(Tween.EaseType.EASE_IN_OUT)
 	tween.tween_callback(hide)
 	
-	if dt.died.is_connected(_hide_hp_bar):
-		dt.died.disconnect(_hide_hp_bar)
+	if dt.died.is_connected(hide_hp_bar):
+		dt.died.disconnect(hide_hp_bar)
 	if dt.suffered.is_connected(_update_bar_animated):
 		dt.suffered.disconnect(_update_bar_animated)
 	if dt.resurrected.is_connected(_update_bar):
--- GUI/hud.gd
+++ GUI/hud.gd
@@ -3,17 +3,13 @@ extends Node
 func _ready() -> void:
 	AppManager.game_paused.connect(func():
 		process_mode = Node.PROCESS_MODE_DISABLED
-		for child in get_children():
-			child.hide()
 		)
 	AppManager.game_unpaused.connect(func():
 		process_mode = Node.PROCESS_MODE_INHERIT
-		for child in get_children():
-			child.show()
 		)
 
 func force_hide_hud_elements() -> void:
-	$BossOverlay._hide_hp_bar()
+	$BossOverlay.hide_hp_bar()
 	$MiniMap.hide_minimap()
 	$Dialog.hide_dialog()
 	$Dialog.hide_hint()
