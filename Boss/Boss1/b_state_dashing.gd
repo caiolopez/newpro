b8a5f0df42afc70508ad2c99ac169418392d239a
caiolopezcomz <caiolopez@gmail.com>
2024-11-21 01:48:37 -0300

    timer to stop boss1 dashing; its a safe net in case the boss somehow dashes through the fences

--- Boss/Boss1/b_state_dashing.gd
+++ Boss/Boss1/b_state_dashing.gd
@@ -1,12 +1,22 @@
 extends BossState
 
+var _timer: Timer
+
+func _ready():
+	_timer = Timer.new()
+	_timer.one_shot = true
+	_timer.wait_time = 1.0
+	_timer.timeout.connect(func(): machine.set_state("BStatePostDash"))
+	add_child(_timer)
 
 func on_enter():
 	$"../../Flier".process_mode = Node.PROCESS_MODE_DISABLED
 	$"../../MoveStraight".process_mode = Node.PROCESS_MODE_INHERIT
 	$"../../MoveStraight".set_static_target_pos_to_curr_target_pos()
-
+	_timer.start()
 
 func on_exit():
+	if _timer.time_left > 0:
+		_timer.stop()
 	$"../../MoveStraight".process_mode = Node.PROCESS_MODE_DISABLED
 	AppManager.camera.shake()
