60458861165824d9d91500f7e70f487c81e55ec8
caiolopezcomz <caiolopez@gmail.com>
2024-11-24 00:41:38 -0300

    positional sound!

--- AudioEmiter/audio_emiter.gd
+++ AudioEmiter/audio_emiter.gd
@@ -1,18 +1,15 @@
 class_name AudioEmiter extends Node2D
 
-## Adds environmental 2D positional sound. Source of the sound is static.
-
-@export var sfx_name: StringName = "" ## The sound emitted by this source, as named in the AudioManager.SFX dictionary.
-@export var max_distance: float = 1000.0 ## The distance at which the sound becomes inaudible.
+@export var sfx_name: StringName = "" ## The sound effect to be played, as named in AudioManager.SFX{}. Remember that looping is set directly on the import of the audio resource.
+@export var max_distance: float = 1000.0 ## The distance after which the audio is inaudible.
+@export var fixed_position: bool = true ## If true, once instantiated, the audio position will be fixed relative to the game level. Set it to false if the audio emiter is moving around, and leave as true if the emiter is static.
 @export_group("Optionals")
-@export var activation_area: Area2D = null ## Optional. The reference for an Area2D that activates this audio emiter upon overlap with hero. If empty, sound source will be always active.
-@export var deactivate_upon_leaving_activation_area: bool = false ## If set to true, leaving the activation area will deactivate source.
-var currently_active: bool = true
-var camera: Camera2D = null
+@export var activation_area: Area2D = null ## If present and valid, the audio emiter will only activate after this Area2D is overlapped by the hero. Leave empty if the sound should be active from the start.
+@export var deactivate_upon_leaving_activation_area: bool = false ## If an Area2D activation area was supplied, setting this variable to true will cause leaving the area to deactivate the audio emiter.
+var currently_active: bool = false
 
 func _ready():
 	if activation_area:
-		currently_active = false
 		activation_area.body_entered.connect(func(body):
 			if body is Hero:
 				_activate()
@@ -22,13 +19,21 @@ func _ready():
 				if body is Hero:
 					_deactivate()
 				)
-	else:
+
+func _process(_delta: float) -> void:
+	AudioManager.update_positional_sfx_position(self, self.global_position)
+
+func _enter_tree():
+	if not activation_area:
 		_activate()
+
+func _exit_tree():
+	_deactivate()
 
 func _activate():
 	currently_active = true
-	AudioManager.play_positional_sfx(sfx_name, global_position, max_distance)
+	AudioManager.play_positional_sfx(sfx_name, global_position, self, max_distance)
 
 func _deactivate():
 	currently_active = false
-	AudioManager.stop_sfx(sfx_name)
+	AudioManager.stop_specific_positional_sfx(self)
