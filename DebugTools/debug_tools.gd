b75e5041e60b8905994db2281f6b4d5a64629b45
Hexany Ives <108251913+HexanyIves@users.noreply.github.com>
2024-11-28 23:14:32 +0300

    Removed play_sfx(&"snare") from debug tools

--- DebugTools/debug_tools.gd
+++ DebugTools/debug_tools.gd
@@ -20,8 +20,6 @@ func input_bin():
 	and Input.is_action_just_pressed("Debug Action 2")\
 	and AppManager.hero.state_machine.current_state.death_prone:
 		AppManager.hero.state_machine.set_state("StateDebug")
-	
-	#if Input.is_action_just_pressed("shoot"): AudioManager.play_sfx(&"snare")
 	#if Input.is_action_just_pressed("jump"): AudioManager.play_music("giorgio")
 
 	#if Input.is_action_just_pressed("shoot"): print("SHOOT!")
