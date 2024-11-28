251fd3b79f843fd20f36419e189c30dcea2e84f3
caiolopezcomz <caiolopez@gmail.com>
2024-11-28 19:58:17 -0300

    added "start_active" bool to audio emiter

--- AudioEmiter/audio_emiter.gd
+++ AudioEmiter/audio_emiter.gd
@@ -4,6 +4,7 @@ class_name AudioEmiter extends Node2D
 @export var max_distance: float = 1000.0 ## The distance after which the audio is inaudible.
 @export var fixed_position: bool = true ## If true, once instantiated, the audio position will be fixed relative to the game level. Set it to false if the audio emiter is moving around, and leave as true if the emiter is static.
 @export var volume_db: float = 0.0
+@export var start_active: bool = true ## If true, will cause the audio to play automatically upon node entering tree.
 @export_group("Optionals")
 @export var activation_area: Area2D = null ## If present and valid, the audio emiter will only activate after this Area2D is overlapped by the hero. Leave empty if the sound should be active from the start.
 @export var deactivate_upon_leaving_activation_area: bool = false ## If an Area2D activation area was supplied, setting this variable to true will cause leaving the area to deactivate the audio emiter.
@@ -13,34 +14,36 @@ func _ready():
 	if activation_area:
 		activation_area.body_entered.connect(func(body):
 			if body is Hero:
-				_activate()
+				activate()
 			)
 		if deactivate_upon_leaving_activation_area:
 			activation_area.body_exited.connect(func(body):
 				if body is Hero:
-					_deactivate()
+					deactivate()
 				)
 	
 	for child in get_parent().get_children():
 		if child is DmgTaker:
-			child.died.connect(_deactivate)
-			child.resurrected.connect(_activate)
+			child.died.connect(deactivate)
+			child.resurrected.connect(activate)
 			break
 
 func _process(_delta: float) -> void:
 	AudioManager.update_positional_sfx_position(self, self.global_position)
 
 func _enter_tree():
-	if not activation_area:
-		_activate()
+	if start_active and not activation_area:
+		activate()
 
 func _exit_tree():
-	_deactivate()
+	deactivate()
 
-func _activate():
+func activate():
+	if currently_active: return
 	currently_active = true
 	AudioManager.play_positional_sfx(sfx_name, global_position, self, max_distance, volume_db)
 
-func _deactivate():
+func deactivate():
+	if not currently_active: return
 	currently_active = false
 	AudioManager.stop_specific_positional_sfx(self)
