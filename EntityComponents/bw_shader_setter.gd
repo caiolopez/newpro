f11f17118026d24ce4f1ffc0f8f42194950158d7
caiolopezcomz <caiolopez@gmail.com>
2024-11-29 17:21:40 -0300

    auto-color grabbing for bwshader setter

--- EntityComponents/bw_shader_setter.gd
+++ EntityComponents/bw_shader_setter.gd
@@ -1,9 +1,11 @@
 class_name BwShaderSetter extends Node
 
+enum COLOR_SOURCES {NOTHING, REGION_THEME_BG, REGION_THEME_FG}
 @export var dark_color: Color = Color.BLACK ## The color that will replace the black pixels. Note: Is overwritten by "Dark Color Name".
-@export var dark_color_name: StringName ## The name of the constant bearing the desired color value, as implemented in the Constants file. This has priority over the color picker value.
 @export var light_color: Color = Color.WHITE ## The color that will replace the white pixels. Note: Is overwritten by "Light Color Name".
+@export var dark_color_name: StringName ## The name of the constant bearing the desired color value, as implemented in the Constants file. This has priority over the color picker value.
 @export var light_color_name: StringName ## The name of the constant bearing the desired color value, as implemented in the Constants file. This has priority over the color picker value.
+@export var inherit_color_from: COLOR_SOURCES = COLOR_SOURCES.NOTHING ## Inheriting colors takes precedence over the other methods.
 @export var transparency: float = 0.0
 @onready var parent: Node = get_parent()
 var mat: ShaderMaterial
@@ -16,10 +18,22 @@ func _ready() -> void:
 
 func _update_color() -> void:
 	if not mat: return
-	if Constants.get(dark_color_name):
-		dark_color = Constants.get(dark_color_name)
-	if Constants.get(light_color_name):
-		light_color = Constants.get(light_color_name)
+
+	match inherit_color_from:
+		COLOR_SOURCES.REGION_THEME_BG:
+			var color_pair = RegionManager.get_region_from_node(self).REGION_COLOR_THEME_BG
+			dark_color = color_pair[0]
+			light_color = color_pair[1]
+		COLOR_SOURCES.REGION_THEME_FG:
+			var color_pair = RegionManager.get_region_from_node(self).REGION_COLOR_THEME_FG
+			dark_color = color_pair[0]
+			light_color = color_pair[1]
+		COLOR_SOURCES.NOTHING:
+			if Constants.get(dark_color_name):
+				dark_color = Constants.get(dark_color_name)
+			if Constants.get(light_color_name):
+				light_color = Constants.get(light_color_name)
+
 	mat.set_shader_parameter("replace_black", dark_color)
 	mat.set_shader_parameter("replace_white", light_color)
 	mat.set_shader_parameter("transparency", transparency)
--- RegionManagement/region.gd
+++ RegionManagement/region.gd
@@ -2,3 +2,5 @@
 class_name Region extends Node
 
 @export var default_checkpoint_trigger: CheckpointTrigger = null ## When to information regarding checkpoints is provided, this will be the starting checkpoint. Use this for the first checkpoint of the game.
+@export var REGION_COLOR_THEME_FG:Array[Color] = [Color("3f160b"), Color("f67f00")]
+@export var REGION_COLOR_THEME_BG:Array[Color] = [Color("3f160b"), Color("f67f00")]
