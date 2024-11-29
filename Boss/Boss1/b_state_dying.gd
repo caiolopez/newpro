bc7465e03122c5bf8f03df17502dec01f3ad33b0
Hexany Ives <108251913+HexanyIves@users.noreply.github.com>
2024-11-29 19:15:25 +0300

    Boss dying state now lasts 4 seconds

--- Boss/Boss1/b_state_dying.gd
+++ Boss/Boss1/b_state_dying.gd
@@ -2,8 +2,8 @@ extends BossState
 
 
 func on_enter():
-	Utils.create_blinking_timer(boss, 0.08, 1)
-	t.wait_time = 1
+	Utils.create_blinking_timer(boss, 0.08, 4)
+	t.wait_time = 4
 	t.start()
 	t.timeout.connect(func():
 		if machine.current_state.name == "BStateDying":
