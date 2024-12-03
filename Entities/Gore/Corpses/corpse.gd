9e36f7d6441ca00627bb7f20c91b7c6807f1e917
Hexany Ives <108251913+HexanyIves@users.noreply.github.com>
2024-12-03 21:51:01 +0300

    EnvironmentalTrigger added for Mess and Corpse entities
    Also reworked the corpse.gd

--- Entities/Gore/Corpses/corpse.gd
+++ Entities/Gore/Corpses/corpse.gd
@@ -1,8 +1,13 @@
-@tool
 extends Sprite2D
 
-@export var particles_enabled: bool = true
+@export var particles: PackedScene
+@export var environment_trigger: PackedScene
 
 func _ready() -> void:
-	if particles_enabled == false:
-		$GPUParticles2D.queue_free()
+	if particles:
+		var particles_instance: GPUParticles2D = particles.instantiate()
+		add_child(particles_instance)
+
+	if environment_trigger:
+		var environment_trigger_instance: EnvironmentalTrigger = environment_trigger.instantiate()
+		add_child(environment_trigger_instance)
