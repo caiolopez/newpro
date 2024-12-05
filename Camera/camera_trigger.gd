9c67a3abf0be222d4fe4eb5b31ca036e24d73717
Caio Lopez <caiolopez@gmail.com>
2024-12-05 17:02:05 -0300

    camera_trigger can now me staged to free() like enemies.

--- Camera/camera_trigger.gd
+++ Camera/camera_trigger.gd
@@ -13,9 +13,13 @@ class_name CameraTrigger extends CollisionShape2D
 			get_parent().queue_redraw()
 
 @export var center_at: CameraLockerController.LockHandles = CameraLockerController.LockHandles.PLACE_OF_CONTACT
+
+var currently_dead: bool = false
 var center_mark: Marker2D = null
 
 func _ready() -> void:
+	Events.hero_respawned_at_checkpoint.connect(_reset_status)
+	Events.hero_reached_checkpoint.connect(_commit_status)
 	for child in get_children():
 		if child is Marker2D:
 			center_mark = child
@@ -26,3 +30,16 @@ func debug_color_changer():
 		Constants.Axes.HORIZONTAL_LOCK: debug_color = Color(0, 0, 1, 0.5)
 		Constants.Axes.VERTICAL_LOCK: debug_color = Color(1, 0, 0, 0.5)
 		Constants.Axes.FULL_LOCK: debug_color = Color(1, 0, 1, 0.5)
+
+func set_currently_dead():
+	currently_dead = true
+	set_deferred("disabled", true)
+
+func _commit_status() -> void:
+	if currently_dead:
+		SaveManager.log_entity_change(self, "dead")
+		queue_free()
+
+func _reset_status() -> void:
+	currently_dead = false
+	set_deferred("disabled", false)
